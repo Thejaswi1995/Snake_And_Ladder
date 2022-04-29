198996955d9b079dcf8b737f4aa15cca9b544051
Thejaswi <thejaswirl187@gmail.com>
2022-04-29 08:39:03 +0530

    Added: Winning Position is added

--- src/com/bridgelabz/snakeandladder/SnakeAndLader.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLader.java
@@ -5,31 +5,41 @@ public class SnakeAndLader {
     static final int NO_PLAY = 1;
     static final int LADDER = 2;
     static final int SNAKE = 3;
-private static int getOption()
-{
-    int option = (int) (Math.random() * 10 % 3) + 1;
-    return option;
-}
+
+    static final int WINNING_POSTION = 100;
+
+    private static int getOption() {
+        int option = (int) (Math.random() * 10 % 3) + 1;
+        return option;
+    }
+
+    private static int dieRoll() {
+        int dieValue = (int) (Math.random() * 10 % 6) + 1;
+        return dieValue;
+    }
 
     public static void main(String[] args) {
 
-        int dieValue = (int) (Math.random() * 10 % 6) + 1;
-        System.out.println("The dice value is :" +dieValue);
+        while (playerPosition < WINNING_POSTION) {
 
-        int option = getOption();
-        System.out.println("Option :" +option);
+            int dieValue = dieRoll();
+            System.out.println("The dice value is :" + dieValue);
 
-        switch(option){
-            case NO_PLAY :
-            break;
-            case LADDER :
-                playerPosition = playerPosition + dieValue ;
-                break;
-            case SNAKE:
-                playerPosition = playerPosition - dieValue ;
-                break;
-            default:
-        }
+            int option = getOption();
+            System.out.println("Option :" + option);
+
+            switch (option) {
+                case NO_PLAY:
+                    break;
+                case LADDER:
+                    playerPosition = playerPosition + dieValue;
+                    break;
+                case SNAKE:
+                    playerPosition = playerPosition - dieValue;
+                    break;
+                default:
+            }
         }
 
     }
+}
