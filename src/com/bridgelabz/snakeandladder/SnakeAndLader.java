dc23577fd6d7b35b6569f497ea5d10d2d1405480
Thejaswi <thejaswirl187@gmail.com>
2022-04-29 08:26:21 +0530

    Added: Check for options

--- src/com/bridgelabz/snakeandladder/SnakeAndLader.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLader.java
@@ -2,11 +2,34 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLader {
     static int playerPosition = 0;
+    static final int NO_PLAY = 1;
+    static final int LADDER = 2;
+    static final int SNAKE = 3;
+private static int getOption()
+{
+    int option = (int) (Math.random() * 10 % 3) + 1;
+    return option;
+}
 
     public static void main(String[] args) {
 
         int dieValue = (int) (Math.random() * 10 % 6) + 1;
         System.out.println("The dice value is :" +dieValue);
+
+        int option = getOption();
+        System.out.println("Option :" +option);
+
+        switch(option){
+            case NO_PLAY :
+            break;
+            case LADDER :
+                playerPosition = playerPosition + dieValue ;
+                break;
+            case SNAKE:
+                playerPosition = playerPosition - dieValue ;
+                break;
+            default:
+        }
         }
 
     }
